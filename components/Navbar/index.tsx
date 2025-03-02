58b16b079fc07543a79d40f019bba6eaabdc1713
Aabith47 <mohammedaabith817@gmai.com>
2025-03-02 16:03:34 +0530

    changes light

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="">
-      <nav className="flex px-6 py-4 justify-around items-center">
+      <nav className="sticky top-0 flex px-8 py-4 justify-between items-center bg-black">
         <div>
-          <Link className="text-green-500 text-2xl" href="#">NATIVE SPICES</Link>
+          <Link className="text-green-500 text-4xl ml-5 font-bold" href="#">NATIVE SPICES</Link>
         </div>
-        <div className="flex">
+        <div className="flex gap-7 text-white text-lg">
             <div>
                 <Link href="#">Home</Link>
             </div>
@@ -23,7 +23,7 @@ const Navbar = () => {
             </div>
         </div>
         <div>
-            <Link href="#">Sign IN</Link>
+            <Link className="mr-5 bg-green-500 rounded rounded-2xl p-3 text-lg" href="#">SIGN IN</Link>
         </div>
       </nav>
     </div>
